0b1e97918cd5396799d5c37a657394799a3ae88b
xenxynon <flynryder427@gmail.com>
2022-08-13 16:54:46 +0530

    Generate signing key

---
+++ generate_key.sh
@@ -0,0 +1,18 @@
+function keygen() {
+    local certs_dir=~/.android-certs
+    [ -z "$1" ] || certs_dir=$1
+    rm -rf "$certs_dir"
+    mkdir -p "$certs_dir"
+    local subject
+    echo "Sample subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
+    echo "Now enter subject details for your keys:"
+    for entry in C ST L O OU CN emailAddress; do
+        echo -n "$entry:"
+        read -r val
+        subject+="/$entry=$val"
+    done
+    for key in certs releasekey platform shared media networkstack testkey; do
+        ./development/tools/make_key "$certs_dir"/$key "$subject"
+    done
+}
+keygen
