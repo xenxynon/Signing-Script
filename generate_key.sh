febadd6fb794f31963d32db36f5d6757a04fe7f4
xenxynon <flynryder427@gmail.com>
2024-05-26 20:41:28 +0530

    Exiting was a bad idea

--- generate_key.sh
+++ generate_key.sh
@@ -45,13 +45,11 @@ function execute_build_signing() {
             echo -e "${GREEN}Build signing completed successfully.${NC}"
         else
             echo -e "${RED}Build signing failed.${NC}"
-            exit 1  # Exit if build signing fails
         fi
     elif [[ "$response" =~ ^(no|n|nope|nah|naw)$ ]]; then
         echo -e "${YELLOW}Skipping build signing as per user request.${NC}"
     else
-        echo -e "${RED}Invalid response. Exiting.${NC}"
-        exit 1
+        echo -e "${RED}Invalid response. Skipping build signing.${NC}"
     fi
 }
 
@@ -81,8 +79,7 @@ function keygen() {
         elif [[ "$response" =~ ^(no|n|nope|nah|naw)$ ]]; then
             echo "Keeping existing directory: $certs_dir"
         else
-            echo "Invalid response. Exiting."
-            exit 1
+            echo "Invalid response. Skipping setup of certificate directory."
         fi
     else
         echo "Setting up new certificate directory..."
@@ -121,15 +118,14 @@ function keygen() {
 
     # Modify make_key script to use 4096 bits instead of 2048 bits
     echo "Updating key length to 4096 bits..."
-    sed -i 's|2048|4096|g' ./development/tools/make_key || exit 1  # Exit if sed command fails
+    sed -i 's|2048|4096|g' ./development/tools/make_key || echo -e "${RED}Failed to update key length.${NC}"
 
     # Generate keys for APEX modules
     print_section "Generating APEX Keys"
-for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do
-    subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN='$apex'/emailAddress=android@android.com'
-    ./development/tools/make_key ~/.android-certs/$apex "$subject"
-    openssl pkcs8 -in ~/.android-certs/$apex.pk8 -inform DER -nocrypt -out ~/.android-certs/$apex.pem
-done
+    for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do
+        ./development/tools/make_key "$certs_dir/$apex" "$subject"
+        openssl pkcs8 -in "$certs_dir/$apex.pk8" -inform DER -nocrypt -out "$certs_dir/$apex.pem"
+    done
 
     print_footer "$certs_dir"
 }
@@ -138,4 +134,4 @@ done
 if [ "$(basename "$0")" == "generate_key.sh" ]; then
     keygen "$@"
     execute_build_signing "$1"
-fi 
+fi
