e0eb2a047caef2fbc08e24d8bc936f9ad6abf5e3
xenxynon <flynryder427@gmail.com>
2025-04-03 14:32:21 +0530

    Allow keeping/choosing key dir

--- generate_key.sh
+++ generate_key.sh
@@ -1,17 +1,26 @@
 function keygen() {
     local certs_dir=~/.android-certs
-    [ -z "$1" ] || certs_dir=$1
-    rm -rf "$certs_dir"
+    [ -n "$1" ] && certs_dir=$1
+
+    if [ -d "$certs_dir" ]; then
+        read -p "Directory $certs_dir already exists. Remove it? (y/N): " choice </dev/tty
+        case "$choice" in
+            [Yy]*) rm -rf "$certs_dir";;
+            *) echo "Using existing directory."; return;;
+        esac
+    fi
+
     mkdir -p "$certs_dir"
+
     local subject=""
     echo "Sample subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
     echo "Now enter subject details for your keys:"
-    
+
     for entry in C ST L O OU CN emailAddress; do
         read -p "$entry: " val </dev/tty
         subject+="/$entry=$val"
     done
-    
+
     for key in bluetooth certs cyngn-app media networkstack otakey nfc platform releasekey sdk_sandbox shared testcert testkey verity; do
         ./development/tools/make_key "$certs_dir"/$key "$subject"
     done
