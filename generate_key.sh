0a0bd6a224f5778c34b251bd9333b484ab932eeb
xenxynon <flynryder427@gmail.com>
2025-04-03 14:30:15 +0530

    Fix curl

--- generate_key.sh
+++ generate_key.sh
@@ -3,14 +3,15 @@ function keygen() {
     [ -z "$1" ] || certs_dir=$1
     rm -rf "$certs_dir"
     mkdir -p "$certs_dir"
-    local subject
+    local subject=""
     echo "Sample subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
     echo "Now enter subject details for your keys:"
+    
     for entry in C ST L O OU CN emailAddress; do
-        echo -n "$entry:"
-        read -r val
+        read -p "$entry: " val </dev/tty
         subject+="/$entry=$val"
     done
+    
     for key in bluetooth certs cyngn-app media networkstack otakey nfc platform releasekey sdk_sandbox shared testcert testkey verity; do
         ./development/tools/make_key "$certs_dir"/$key "$subject"
     done
